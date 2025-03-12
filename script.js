8eaa36f23ed7e67e43ecb85f6643c1af5d2e4d59
navyavarma08 <navyaganna2002@gmail.com>
2025-03-12 10:01:14 +0530

    Navya

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // alter 
-// function showMessage() {
-//     let message = "Hello!";
-//     alert(message);
-//   }
-//   showMessage(); // Displays: Hello!
-//   alert(message);
+ function showMessage() {
+    let message = "Hello!";
+    alert(message);
+  }
+  showMessage(); // Displays: Hello!
+  alert(message);
 
 //  RestParameters 
-// function showName(firstName, lastName, ...titles) {
-//     console.timeLog(firstName + ' ' + lastName); // "Julius Caesar"
-//     console.log(titles[0]); // "Consul"
-//     console.log(titles.length); // 2
-//   }
+ function showName(firstName, lastName, ...titles) {
+    console.timeLog(firstName + ' ' + lastName); // "Julius Caesar"
+    console.log(titles[0]); // "Consul"
+     console.log(titles.length); // 2
+   }
   
-//   showName("Julius", "Caesar", "Consul", "Imperator");
+   showName("Julius", "Caesar", "Consul", "Imperator");
   
 // query selector
 
