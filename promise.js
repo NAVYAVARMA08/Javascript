d991d66644460d21b9f1cc886a44637aba465d05
navyavarma08 <navyaganna2002@gmail.com>
2025-03-13 09:38:21 +0530

    Navya

--- promise.js
+++ promise.js
@@ -10,21 +10,38 @@
 //  .catch((error)=>{
 // console.log(error + "reject")})
 
-const mypromise = new Promise((resolve,reject)=>{
-    const randomnum = Math.random();
-    if (randomnum>0.5){
-        resolve("Number greater ")
-    } else{
-        reject("smaller")
-    }
-    })
-    mypromise
-    .then((value)=>{
-        console.log(value)
-        return "Hello new return"
-    })
-    .then(mess=>{
-        console.log(mess)
-    })
-    .catch((error)=>{
-        console.log(error)})
+// const mypromise = new Promise((resolve,reject)=>{
+//     const randomnum = Math.random();
+//     if (randomnum>0.5){
+//         resolve("Number greater ")
+//     } else{
+//         reject("smaller")
+//     }
+//     })
+//     mypromise
+//     .then((value)=>{
+//         console.log(value)
+//         return "Hello new return"
+//     })
+//     .then(mess=>{
+//         console.log(mess)
+//     })
+//     .catch((error)=>{
+//         console.log(error)})
+
+// promise.all()
+let p1 = new Promise((resolve,reject)=>{
+    setTimeout(resolve("first"),1000)
+})
+let p2 = new Promise((resolve,reject)=>{
+    setTimeout(resolve("second"),2000)
+    
+})
+let p3 = new Promise((resolve,reject)=>{
+    setTimeout(resolve("Third"),3000)
+})
+Promise.all([p1,p2,p3])
+.then((value)=>
+    {console.log(value)})
+.catch((error)=>
+    {console.log(error)})
