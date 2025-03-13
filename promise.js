9779e278cfd2449c999dbbe5738055c9f969a36f
navyavarma08 <navyaganna2002@gmail.com>
2025-03-13 09:55:25 +0530

    Navya

--- promise.js
+++ promise.js
@@ -29,6 +29,8 @@
 //     .catch((error)=>{
 //         console.log(error)})
 
+// advanced promises
+
 // promise.all()
 // let p1 = new Promise((resolve,reject)=>{
 //     setTimeout(resolve("first"),1000)
@@ -46,18 +48,53 @@
 // .catch((error)=>
 //     {console.log(error)})
 
-let p1 = new Promise((resolve,reject)=>{
-    setTimeout(resolve("first"),1000)
-})
-let p2 = new Promise((resolve,reject)=>{
-    setTimeout(reject("second"),2000)
+// promise.allSettled()
+
+// let p1 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("first"),1000)
+// })
+// let p2 = new Promise((resolve,reject)=>{
+//     setTimeout(reject("second"),2000)
     
+// })
+// let p3 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("Third"),3000)
+// })
+// Promise.allSettled([p1,p2,p3])
+// .then((value)=>
+//     {console.log(value)})
+// .catch((error)=>
+//     {console.log(error)})
+
+ let p1 = new Promise((resolve,reject)=>{
+    setTimeout(resolve("first"),1000)
+ })
+ let p2 = new Promise((resolve,reject)=>{
+    setTimeout(reject("second"),2000) 
 })
 let p3 = new Promise((resolve,reject)=>{
     setTimeout(resolve("Third"),3000)
 })
-Promise.allSettled([p1,p2,p3])
+Promise.any([p1,p2,p3])
 .then((value)=>
     {console.log(value)})
 .catch((error)=>
     {console.log(error)})
+
+// promise.race()
+
+// let p1 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("first"),1000)
+// })
+// let p2 = new Promise((resolve,reject)=>{
+//     setTimeout(reject("second"),2000)
+    
+// })
+// let p3 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("Third"),3000)
+// })
+// Promise.race([p1,p2,p3])
+// .then((value)=>
+//     {console.log(value)})
+// .catch((error)=>
+//     {console.log(error)})
